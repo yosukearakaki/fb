ded53349552c52c921c3773348cbb73e3f747d3f
Yosuke Arakaki <yosuke@quipper.com>
2017-06-17 12:44:45 +0900

    mail authentification and errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy]
 
   root 'top#index'
+
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

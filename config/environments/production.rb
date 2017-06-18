8b9d4d54dc02acd5595563d34c749b15bd818cce
Yosuke Arakaki <yosuke@quipper.com>
2017-06-18 15:18:57 +0900

    change sendgrid direction from heroku to aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://secure-falls-75643.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://13.113.215.194/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {

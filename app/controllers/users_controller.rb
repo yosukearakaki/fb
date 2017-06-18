81ea9e525c55f3820d141d377a613e96c37bfc07
Yosuke Arakaki <yosuke@quipper.com>
2017-06-18 10:54:41 +0900

    fix follow feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
+
+  def show
+    @followers = @user.followers
+    @followed_users = @user.followed_users
+  end
+
+  def edit
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 end
